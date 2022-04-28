f5f82082a565c6fd3508fb0ec053dae964312bed
gabyagaa <agaa@ier.unam.mx>
2022-04-28 12:07:35 -0500

    resuelto los warnings en .py

--- IDF/LES_python/LES_AC.py
+++ IDF/LES_python/LES_AC.py
@@ -2,7 +2,7 @@ from pyenergyplus.plugin import EnergyPlusPlugin
 
 class LES_AC(EnergyPlusPlugin):
 
-    def on_begin_zone_timestep_before_init_heat_balance(self, state):
+     def on_begin_zone_timestep_before_init_heat_balance(self, state) -> int:
        #SENSORES
         S_To = self.api.exchange.get_variable_handle(state, "Site Outdoor Air Drybulb Temperature", "Environment")
         V_To = self.api.exchange.get_variable_value(state, S_To)
@@ -11,17 +11,17 @@ class LES_AC(EnergyPlusPlugin):
         V_TZ_habitacion = self.api.exchange.get_variable_value(state, S_TZ_habitacion)
         
         #ACTUADORES
-        A_Tclima_cool = self.api.exchange.get_actuator_handle(state, "SCHEDULE:COMPACT", "SCHEDULE VALUE" , "Cooling")
-        self.api.exchange.set_actuator_value(state, A_Tclima_cool, V_To)
-        
-        A_Tclima_hot = self.api.exchange.get_actuator_handle(state, "SCHEDULE:COMPACT", "SCHEDULE VALUE" , "Heating")
-        self.api.exchange.set_actuator_value(state, A_Tclima_hot, V_To)
-      
-        A_Tguarda_cool = self.api.exchange.get_actuator_handle(state, "SCHEDULE:COMPACT", "SCHEDULE VALUE" , "Cooling_2")
-        self.api.exchange.set_actuator_value(state, A_Tguarda_cool, V_TZ_habitacion)
-        
-        A_Tguarda_hot = self.api.exchange.get_actuator_handle(state, "SCHEDULE:COMPACT", "SCHEDULE VALUE" , "Heating_2")
-        self.api.exchange.set_actuator_value(state, A_Tguarda_hot, V_TZ_habitacion)
+        if 'handles_done' not in self.data:
+            self.A_Tclima_cool = self.api.exchange.get_actuator_handle(state, "SCHEDULE:COMPACT", "SCHEDULE VALUE" , "Cooling")
+            self.A_Tclima_hot = self.api.exchange.get_actuator_handle(state, "SCHEDULE:COMPACT", "SCHEDULE VALUE" , "Heating")
+            self.A_Tguarda_cool = self.api.exchange.get_actuator_handle(state, "SCHEDULE:COMPACT", "SCHEDULE VALUE" , "Cooling_2")
+            self.A_Tguarda_hot = self.api.exchange.get_actuator_handle(state, "SCHEDULE:COMPACT", "SCHEDULE VALUE" , "Heating_2")
+            self.data['handles_done'] = True
+            
+        self.api.exchange.set_actuator_value(state, self.A_Tclima_cool, V_To)
+        self.api.exchange.set_actuator_value(state, self.A_Tclima_hot, V_To)
+        self.api.exchange.set_actuator_value(state, self.A_Tguarda_cool, V_TZ_habitacion)
+        self.api.exchange.set_actuator_value(state, self.A_Tguarda_hot, V_TZ_habitacion)
         
       
         
